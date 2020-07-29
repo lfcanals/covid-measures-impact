1a2e38d7577c5bf24f7ce8036858e8924fba88f1
lfcanals <luisfernando.canals@bbva.com>
2020-07-29 18:07:35 +0200

    Add Y to plot clouds

---
+++ src/to_plot.py
@@ -0,0 +1,36 @@
+import sys
+import random
+import csv
+import datetime
+
+#
+# parameter:
+#   inputFileName: csv result of process with columns
+#       date,location,class,val1,val2,...
+#
+#   outputFileName: csv SEPARATED-BY-SEMICOLON (;) with these values:
+#         date,location,class,val1,val2...,Y
+#       Y will be set randomly in the environment of class to not mix with
+#       other classes.
+#
+def to_plot(inputFileName, outputFileName):
+  with open(inputFileName) as inputFile, open(outputFileName,'w') as outputFile:
+    reader = csv.reader(inputFile)
+    header = next(reader)
+    headerMap = {}
+    for i in range(len(header)): headerMap[header[i].lower()] = i
+    headerMap['Y'] = len(header)
+
+    writer = csv.writer(outputFile, delimiter=';')
+    writer.writerow(headerMap.keys())
+
+    for row in reader:
+      Y = int(row[headerMap['class']])
+      Y += Y + (random.random()-0.5)*2
+      resultingRow = row
+      resultingRow.append(Y)
+
+      writer.writerow(resultingRow)
+
+
+to_plot(sys.argv[1], sys.argv[2])
